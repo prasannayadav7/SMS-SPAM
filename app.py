66249bcce111def0d75f15e2d56ebd1451f657a9
udayyadav67 <udayyadav4962@gmail.com>
2025-06-29 20:25:55 +0530

    commit

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ tk = pickle.load(open("vectorizer.pkl", 'rb'))
 model = pickle.load(open("model.pkl", 'rb'))
 
 st.title("SMS Spam Detection Model")
-st.write("*Made by Edunet Foundation*")
+st.write("*Made by Laxmi prasanna*")
     
 
 input_sms = st.text_input("Enter the SMS")
